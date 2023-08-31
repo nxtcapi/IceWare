f507ce59a31f60202422150baa60468a2ed63c3f
capi <89797002+nxtcapi@users.noreply.github.com>
2023-08-31 18:27:34 +0200

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,11 +1,3 @@
-local games = {
-    [{1554960397}] = "https://raw.githubusercontent.com/nxtcapi/IceWare/main/loader.lua", -- Car Dealership Tycoon
-}
-
-for ids, url in next, games do
-    if table.find(ids, game.PlaceId) then
-        loadstring(game:HttpGet(url))()
-        print(game.PlaceId)
-        break
-    end
+if game.PlaceId == 1554960397 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/nxtcapi/IceWare/main/games/car-dealership-tycoon.lua",true))()
 end
