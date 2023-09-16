3c224ea16d7180766c057d989969cc5b1aeefc11
capi <89797002+nxtcapi@users.noreply.github.com>
2023-09-16 22:44:09 +0200

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,5 +1,6 @@
 local games = {
     [{1554960397}] = "https://raw.githubusercontent.com/nxtcapi/IceWare/main/games/car-dealership-tycoon.lua", -- Car Dealership Tycoon
+    [{13864667823}] = "https://raw.githubusercontent.com/nxtcapi/IceWare/main/games/break%20in%202", -- Break in 2
 }
 
 for ids, url in next, games do
