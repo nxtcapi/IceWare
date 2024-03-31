f4b66e4ae6467a61eb589cf049980522aec51552
capi <89797002+nxtcapi@users.noreply.github.com>
2024-03-31 16:40:07 +0200

    Update car-dealership-tycoon.lua

--- games/car-dealership-tycoon.lua
+++ games/car-dealership-tycoon.lua
@@ -1,1 +1,21 @@
-loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/a4315fa21279b87ad646fd70daf38199.lua"))()
+
+repeat wait() until game.Players.LocalPlayer.Character
+repeat wait() until game:IsLoaded() wait()
+
+
+game:GetService("Players").LocalPlayer.Idled:connect(function()
+game:GetService("VirtualUser"):ClickButton2(Vector2.new())
+end)
+
+local games = {
+    [{1554960397}] = "https://raw.githubusercontent.com/SkiddyScripts/IceWare/main/Games/Car-Dealership-Tycoon/Main.lua", -- Car Dealership Tycoon
+}
+
+for ids, url in next, games do
+    if table.find(ids, game.PlaceId) then
+        wait()
+        loadstring(game:HttpGet(url))()
+        warn(game.PlaceId)
+        break
+    end
+end
