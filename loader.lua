b6c53b79e789138f81112db55a3e886e3b3da05b
capi <89797002+nxtcapi@users.noreply.github.com>
2024-03-31 16:39:46 +0200

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,13 +1,21 @@
+
+repeat wait() until game.Players.LocalPlayer.Character
+repeat wait() until game:IsLoaded() wait()
+
+
+game:GetService("Players").LocalPlayer.Idled:connect(function()
+game:GetService("VirtualUser"):ClickButton2(Vector2.new())
+end)
+
 local games = {
-    [{1554960397}] = "https://raw.githubusercontent.com/nxtcapi/IceWare/main/games/car-dealership-tycoon.lua", -- Car Dealership Tycoon
-    [{13864667823}] = "https://raw.githubusercontent.com/nxtcapi/IceWare/main/games/break%20in%202", -- Break in 2
+    [{1554960397}] = "https://raw.githubusercontent.com/SkiddyScripts/IceWare/main/Games/Car-Dealership-Tycoon/Main.lua", -- Car Dealership Tycoon
 }
 
 for ids, url in next, games do
     if table.find(ids, game.PlaceId) then
         wait()
         loadstring(game:HttpGet(url))()
-        print(game.PlaceId)
+        warn(game.PlaceId)
         break
     end
 end
