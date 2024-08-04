317bddd37fb8ccfe2988714d51ea29353d2f7cab
capi <89797002+nxtcapi@users.noreply.github.com>
2024-08-04 17:57:06 +0200

    Update car-dealership-tycoon.lua

--- games/car-dealership-tycoon.lua
+++ games/car-dealership-tycoon.lua
@@ -1,1 +1,6 @@
-
+setclipboard("https://discord.gg/XYYDyPaeXj")
+game.StarterGui:SetCore("SendNotification", {
+Title = "99",
+Text = "Script offline, discord has been copied to clibpard.",
+Duration = 9e9,
+})
